a8468b86bf3ee145ea9ad157d222ae9893ef9c4c
syarizx-30 <241051004@students.unsurya.ac.id>
2026-05-02 20:17:04 +0700

    implement database schemas

---
+++ src/db/index.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined');
+}
+
+const sql = neon(process.env.DATABASE_URL);
+export const index = drizzle(sql);
+// pool is not used in neon-http but exported as null for consistency in index.ts
+export const pool = null;
