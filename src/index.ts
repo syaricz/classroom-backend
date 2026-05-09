c98e6c7e448c8ea37d0447eefba6785e345d5ae5
syarizx-30 <241051004@students.unsurya.ac.id>
2026-05-09 09:00:00 +0700

    implement GET all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+
+import subjectsRouter from "./routes/subjects";
 
 const app = express();
 const port = 8000;
 
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}))
+
 app.use(express.json());
+
+app.use('/api/subjects', subjectsRouter)
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Classroom API');
