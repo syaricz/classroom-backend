2be0dc4f2ec83a44175d99e92ab3be658570e5b1
syarizx-30 <241051004@students.unsurya.ac.id>
2026-05-13 09:27:45 +0700

    implement security middleware

---
+++ src/config/arcjet.ts
@@ -0,0 +1,26 @@
+import arcjet, { shield, detectBot, tokenBucket, slidingWindow } from "@arcjet/node";
+
+if (!process.env.ARCJET_KEY && process.env.NODE_ENV !== "test") {
+    throw new Error("ARCJET_KEY env is required");
+}
+
+const aj = arcjet({
+    key: process.env.ARCJET_KEY,
+    rules: [
+        shield({ mode: "LIVE" }),
+        detectBot({
+            mode: "LIVE",
+            allow: [
+                "CATEGORY:SEARCH_ENGINE",
+                "CATEGORY:PREVIEW",
+            ],
+        }),
+        slidingWindow({
+            mode: 'LIVE',
+            interval: '2s',
+            max: 5,
+        })
+    ],
+});
+
+export default aj;
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 import subjectsRouter from "./routes/subjects";
+import securityMiddleware from "./middleware/security";
 
 const app = express();
 const port = 8000;
@@ -15,6 +16,8 @@ app.use(cors({
 }))
 
 app.use(express.json());
+
+app.use(securityMiddleware);
 
 app.use('/api/subjects', subjectsRouter)
 
